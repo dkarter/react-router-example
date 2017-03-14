45857582908ef89af5c3c743868dd407b02e3f99
Dorian Karter <dkarter@gmail.com>
2017-03-14 15:47:52 -0500

    Use URL Parameters with React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { startCase } from 'lodash';
 
 import './App.css';
 
@@ -12,13 +13,20 @@ const Links = () => {
     </nav>
   );
 };
+
+const PageComponent = ({ match }) => {
+  return (
+    <h1>
+      PAGE: {startCase(match.params.page) || 'Home'}<br />
+      SUBPAGE: {startCase(match.params.subpage)}
+    </h1>
+  );
+};
 const App = () => (
   <Router>
     <div>
       <Links />
-      <Route exact path="/" render={() => <h1>Home</h1>} />
-      <Route path="/about" render={() => <h1>About</h1>} />
-      <Route path="/contact" render={() => <h1>Contact</h1>} />
+      <Route exact path="/:page?/:subpage?" component={PageComponent} />
     </div>
   </Router>
 );
